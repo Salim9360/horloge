5b2184c820b2ccdb3cab8ee12c1624669623bd87
Salim9360 <salim.bahache@latoile.em-lyon.com>
2025-05-19 10:36:59 +0200

    hehe

--- script.js
+++ script.js
@@ -2,55 +2,62 @@
 function changeStyleBasedOnTime() {
 
     //* 1. Ici on veut créer la variable currentHour qui contient l'heure actuelle en direct. On peut l'obtenir grâce à la fonction interne à Javascript : new Date().getHours(). Le résultat est une valeur de type number comprise entre 0 et 23
-
-
-
-
-
+    let currentHour = new Date().getHours();
+    //console.log(currentHour);
 
     //* 2. On crée la variable sayHi qui correspond à la balise ayant l'id 'say-hi'
-
-
-
-
+    
+    let sayHi = document.getElementById('say-hi')
+    //console.log(sayHi);
 
     //**! 3. ICI on veut une condition pour afficher Bonjour ou Bonsoir en fonction de l'heure de la journée */
         //* Entre 5h et 18h, on affiche "Bonjour !" en injectant du code dans l'id say-hi
 
-
-
-
-       //* Entre 18h et 5h, on affiche "Bonsoir !" en injectant du code dans l'id say-hi
-
-
-
+        if (currentHour >= 5 && currentHour <= 18) {
+            sayHi.innerHTML = "Bonjour !"
+        } 
+        else {
+            //* Entre 18h et 5h, on affiche "Bonsoir !" en injectant du code dans l'id say-hi
+            sayHi.innerHTML = "Bonsoir !"
+        }
 
 
     //* 4. On créé la variable element qui correspond à la balise ayant l'id 'time-based-style'
 
-
+        let element = document.getElementById('time-based-style');
 
 
 
     //* 5. On réinitialise les classes sur element, c'est-à-dire que l'on .remove toutes les classes potentielles qui ont été ajoutées sur cet élément
 
-
-
-
-
-
+    element.classList.remove("matin", "aurore", "apres-midi", "soiree", "nuit")
 
     //**! 6. ICI on veut une condition pour appliquer la classe appropriée en fonction de la variable currentHour (l'heure actuelle) */
     //* On va appliquer la classe aurore, matin, apres-midi, soiree, ou nuit en fonction de l'heure de la journée
     //* Pour faire cela on crée une condition de la variable currentHour (sa valeur est comprise entre 0 et 23)
     //* On ajoute la classe css appropriée en fonction de l'heure avec la code suivant : 
 
-
-            //* Aurore : 5h-8h
-            //* Matin : 8h-12h
-            //* Après-midi : 12h-18h
-            //* Soirée : 18h-22h
-            //* Nuit : 22h-5h
+    
+    //* Aurore : 5h-8h
+    if (currentHour >= 5 && currentHour <= 8) {
+        element.classList.add('aurore')
+    }
+    //* Matin : 8h-12h
+    else if(currentHour >= 8 && currentHour <= 12){
+        element.classList.add('matin')
+    }
+    //* Après-midi : 12h-18h
+    else if(currentHour >= 12 && currentHour <= 18){
+        element.classList.add('apres-midi')
+    }
+    //* Soirée : 18h-22h
+    else if(currentHour >= 18 && currentHour <= 22){
+        element.classList.add('soiree')
+    }else{
+        //* Nuit : 22h-5h
+        element.classList.add('nuit')
+    }
+    
 
 
 
